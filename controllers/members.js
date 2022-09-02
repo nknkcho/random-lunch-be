5c9275547d668434564e80243697e6e884eac957
nknkcho <srirachacho@gmail.com>
2022-09-03 02:54:22 +0900

    Feature: 컨트롤러 폴더 추가

---
+++ controllers/members.js
@@ -0,0 +1,13 @@
+const createMember = (req, res) => {
+  return;
+};
+
+const getMembers = (req, res) => {
+  return res.send('hi members');
+};
+
+const deleteMember = (req, res) => {
+  return;
+};
+
+export default { createMember, getMembers, deleteMember };
