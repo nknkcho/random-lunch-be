dd021f2838a9bbca3253c34b88e1152f54482ff1
nknkcho <srirachacho@gmail.com>
2022-09-06 00:21:12 +0900

    Feat: deleteMember 컨트롤러 구현, createMember 컨트롤러 내 에러 헨들링 보완

--- controllers/members.js
+++ controllers/members.js
@@ -4,11 +4,17 @@ import statusCodes from '../config/statusCodes';
 const createMember = async (req, res) => {
   try {
     const { name } = req.body;
+    if (name.length === 0) {
+      return await Promise.reject({
+        message: 'BAD_REQUEST',
+        status: statusCodes.BAD_REQUEST,
+      });
+    }
     const searchName = await Member.searchName(name);
-    console.log(searchName);
     if (searchName.length > 0) {
-      return res.status(statusCodes.CONFLICT).send({
+      return await Promise.reject({
         message: 'CONFLICT',
+        status: statusCodes.CONFLICT,
       });
     }
     const data = await Member.create(name);
@@ -16,7 +22,9 @@ const createMember = async (req, res) => {
       data,
     });
   } catch (error) {
-    console.log(error);
+    res.status(error.status || statusCodes.INTERNAL_SERVER).send({
+      message: error.message || 'UNKNOWN_ERROR',
+    });
   }
 };
 
@@ -28,11 +36,22 @@ const getMembers = (req, res) => {
   }
 };
 
-const deleteMember = (req, res) => {
+const deleteMember = async (req, res) => {
   try {
-    return res.send('hi members');
+    const { name } = req.params;
+    const searchName = await Member.searchName(name);
+    if (searchName.length === 0) {
+      return await Promise.reject({
+        message: 'NOT_FOUND',
+        status: statusCodes.NOT_FOUND,
+      });
+    }
+    await Member.delete(name);
+    return res.status(statusCodes.NO_CONTENT);
   } catch (error) {
-    console.log(error);
+    res.status(error.status || statusCodes.INTERNAL_SERVER).send({
+      message: error.message || 'UNKNOWN_ERROR',
+    });
   }
 };
 
