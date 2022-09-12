4e3b69e747e9978868b0f6673bcc438f41268f60
Namkyung <srirachacho@gmail.com>
2022-09-12 22:19:49 +0900

    Refactor: CORS 에러 해결을 위한 라이브러리 설치 및 연결

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import membersRouter from './routers/members.js';
 
 const app = express();
 const port = 3100;
 
 app.use(express.json());
+app.use(cors());
 app.use('/members', membersRouter);
 app.listen(port, () => {
   console.log('server is running..');
